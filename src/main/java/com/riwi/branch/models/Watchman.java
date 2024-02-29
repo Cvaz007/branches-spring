7ac3ef3e54731ef3a33638ebea26e5f195348f8d
cvaz007 <sebastianmoreno301177@correo.itm.edu.co>
2024-02-29 13:04:20 -0500

    principal configuration

---
+++ src/main/java/com/riwi/branch/models/Watchman.java
@@ -0,0 +1,11 @@
+package com.riwi.branch.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Watchman {
+    @Id
+    private String identificationNumber;
+
+}
